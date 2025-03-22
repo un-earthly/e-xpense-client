54e196829b9ab3f8cd6c41e2b6e50bdf95c88b2c
un-earthly <88502576+un-earthly@users.noreply.github.com>
2025-03-22 06:18:45 +0600

    auth pages designs

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -46,7 +46,7 @@ export default function LandingPage() {
                         ))}
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                        <a href="/login" className="text-sm/6 font-semibold text-gray-900">
                             Log in <span aria-hidden="true">&rarr;</span>
                         </a>
                     </div>
--- src/route.tsx
+++ src/route.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 export default function AppRouter() {
     return (
         <Routes>
             <Route index element={<App />} />
             <Route path="about" element={<div >about</div>} />
-
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegistrationPage />} />
             <Route element={<div >Layout</div>}>
                 <Route path="login" element={<div >login</div>} />
                 <Route path="register" element={<div >register</div>} />
